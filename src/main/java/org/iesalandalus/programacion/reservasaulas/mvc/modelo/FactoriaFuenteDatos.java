530aedebcbaeefd8348dc5591cb9fd1a59ed00b7
heleven-pc <heleven-pc@192.168.22.11>
2022-03-13 17:53:32 +0100

    Opcion de fuente de datos ficheros

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/FactoriaFuenteDatos.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/FactoriaFuenteDatos.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.reservasaulas.mvc.modelo;
 
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.negocio.ficheros.FactoriaFuenteDatosFicheros;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.negocio.memoria.FactoriaFuenteDatosMemoria;
 
 
@@ -9,7 +10,14 @@ public enum FactoriaFuenteDatos {
 		public IFuenteDatos crear() {
 			return new FactoriaFuenteDatosMemoria();
 		}
-	};
+	},
+	
+	FICHEROS(){
+		public IFuenteDatos crear() {
+			return new FactoriaFuenteDatosFicheros();
+		}
+	},
+	;
 	
 	FactoriaFuenteDatos() {
 	} 
