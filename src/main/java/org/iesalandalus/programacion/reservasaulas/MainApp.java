6486a85fb5e288dfa3bc5dd207485d8874781990
heleven-pc <heleven-pc@192.168.22.11>
2022-03-13 17:53:44 +0100

    seleccionar opcion de fuente de datos

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -5,21 +5,34 @@ import org.iesalandalus.programacion.reservasaulas.mvc.modelo.FactoriaFuenteDato
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.IModelo;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.Modelo;
 import org.iesalandalus.programacion.reservasaulas.mvc.vista.IVista;
+import org.iesalandalus.programacion.reservasaulas.mvc.vista.Opcion;
 import org.iesalandalus.programacion.reservasaulas.mvc.vista.Vista;
+import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
 		
-		IModelo modelo;
-		IVista vista;
-		
-		vista = new Vista();
+		IModelo modelo = null;
+		IVista vista = new Vista();
 		
 		// Creamos el Modelo pasandole como parametro la fuente de datos
-		// En este caso solo tenemos una opcion por lo que la pasamos directamente sin preguntar
-		modelo = new Modelo(FactoriaFuenteDatos.MEMORIA.crear());
+		// En este caso preguntamos si queremos los datos de memoria o de ficheros
+		int respuesta = 0;
+		do {
+			System.out.println("¿Que datos quieres? ");
+			for (FactoriaFuenteDatos opcion: FactoriaFuenteDatos.values()) {
+	            System.out.println(opcion); 
+			}
+			respuesta = Entrada.entero();
+		} while (respuesta < 0 || respuesta > 1);
+		
+		// segun la respuesta llamamo a una opcion u otra de fuente de datos
+		if (respuesta == 0)		
+			modelo = new Modelo(FactoriaFuenteDatos.MEMORIA.crear());
+		else if (respuesta == 1)
+			modelo = new Modelo(FactoriaFuenteDatos.FICHEROS.crear());
 		
 		try {
 			new Controlador(modelo, vista);
